ae6f5dd5d9348108caf7bec7a4fa948c11b15aa1
essay <essaybeihai@gmail.com>
2021-10-11 15:20:53 +0800

    初始化

---
+++ src/main/java/cn/imessay/speedtest/SpeedtestApplication.java
@@ -0,0 +1,13 @@
+package cn.imessay.speedtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeedtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/imessay/speedtest/SpeedtestApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.imessay.speedtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
