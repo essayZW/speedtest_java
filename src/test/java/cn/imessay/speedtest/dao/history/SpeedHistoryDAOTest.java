ecd6dd297f8d99e55a2acdbea9ea2cb230a13e6a
essay <essaybeihai@gmail.com>
2021-10-21 18:59:37 +0800

    构建用户查询个人测速历史接口

--- src/test/java/cn/imessay/speedtest/dao/history/SpeedHistoryDAOTest.java
+++ src/test/java/cn/imessay/speedtest/dao/history/SpeedHistoryDAOTest.java
@@ -1,11 +1,14 @@
 package cn.imessay.speedtest.dao.history;
 
+import cn.imessay.speedtest.pojo.dto.SpeedHistoryDTO;
 import cn.imessay.speedtest.pojo.dto.UserDTO;
+import cn.imessay.speedtest.pojo.vo.PageQueryVO;
 import com.alibaba.fastjson.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Random;
 
 @SpringBootTest
@@ -29,5 +32,18 @@ class SpeedHistoryDAOTest {
         UserDTO userDTO = new UserDTO();
         userDTO.setId(random.nextInt());
         System.out.println(speedHistoryDAO.insert(speedHistoryDO, userDTO));
+    }
+
+    @Test
+    void queryByUser() {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(1);
+        PageQueryVO pageQueryVO = new PageQueryVO();
+        pageQueryVO.setIndex(2);
+        pageQueryVO.setSize(3);
+        List<SpeedHistoryDTO> speedHistoryDTOS = speedHistoryDAO.queryByUser(userDTO, pageQueryVO);
+        for (SpeedHistoryDTO speedHistoryDTO : speedHistoryDTOS) {
+            System.out.println(speedHistoryDTO);
+        }
     }
 }
