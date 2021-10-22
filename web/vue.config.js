0940afb9cd30b9fdb5a24fac28b365f1f62cf018
essay <essaybeihai@gmail.com>
2021-10-22 10:51:11 +0800

    调整vue项目设置

---
+++ web/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    assetsDir : 'static',
+    pages: {
+        index: {
+            entry: 'src/pages/index/main.js',
+            template: 'public/index.html',
+            filename: 'index.html'
+        }
+    }
+}
